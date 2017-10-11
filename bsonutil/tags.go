da6396c97c302e4d97fa15f3877025475df7f027
Sam Kleinman <sam@10gen.com>
2017-10-11 10:42:21 -0400

    add bsonutil tests and update from evg

--- bsonutil/tags.go
+++ bsonutil/tags.go
@@ -9,20 +9,31 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 // Tag returns the value of the "bson" tag for the given struct field name of
 // the "data" struct, stripping any tag modifiers such as "omitempty".
 // Returns the empty string if there is no tag, and an error if the field
 // does not exist in the struct.
+//
+// If data is a slice of structs, this check applies to the type of
+// struct in the slice.
 func Tag(data interface{}, fieldName string) (string, error) {
 	dataType := reflect.TypeOf(data)
-	if dataType.Kind() != reflect.Struct {
-		return "", fmt.Errorf("must pass in a struct data type")
+	if dataType.Kind() == reflect.Slice {
+		dataType = dataType.Elem()
 	}
+
+	if dataType.Kind() != reflect.Struct {
+		return "", errors.Errorf("must pass in a struct data type [%T]", data)
+	}
+
 	field, found := dataType.FieldByName(fieldName)
 	if !found {
-		return "", fmt.Errorf("struct does not have a field %v", fieldName)
+		return "", errors.Errorf("struct of type '%T' does not have a field %s",
+			data, fieldName)
 	}
 	tag := field.Tag.Get("bson")
 
@@ -39,7 +50,7 @@ func Tag(data interface{}, fieldName string) (string, error) {
 func MustHaveTag(data interface{}, fieldName string) string {
 	tagValue, err := Tag(data, fieldName)
 	if err != nil {
-		panic(fmt.Sprintf("error getting bson tag: %v", err))
+		panic(fmt.Sprintf("error getting bson tag: %s", err.Error()))
 	}
 	if tagValue == "" {
 		panic(fmt.Sprintf("field %v cannot have an empty bson tag", fieldName))
