dc05adaee8c421edcb9a6db1ac9986a1f03f7455
Sam Kleinman <sam@10gen.com>
2018-10-30 14:33:25 -0400

    add upsert opt

---
+++ db/buffered_upsert.go
@@ -0,0 +1,193 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/mongodb/grip"
+	"github.com/pkg/errors"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type anserBufUpsertImpl struct {
+	opts    BufferedWriteOptions
+	db      Database
+	cancel  context.CancelFunc
+	upserts chan upsertOp
+	flusher chan chan error
+	closer  chan chan error
+	err     chan error
+}
+
+type upsertOp struct {
+	query  interface{}
+	record interface{}
+}
+
+func (bu *anserBufUpsertImpl) start(ctx context.Context) {
+	timer := time.NewTimer(bu.opts.Duration)
+	defer timer.Stop()
+	ops := 0
+	bulk := bu.db.C(bu.opts.Collection).Bulk()
+	catcher := grip.NewBasicCatcher()
+
+bufferLoop:
+	for {
+		select {
+		case <-ctx.Done():
+			if ops > 0 {
+				catcher.Add(errors.Errorf("buffered upsert has %d pending inserts", ops))
+			}
+
+			bu.err <- catcher.Resolve()
+			break bufferLoop
+		case <-timer.C:
+			if ops > 0 {
+				_, err := bulk.Run()
+				catcher.Add(err)
+				ops = 0
+			}
+			timer.Reset(bu.opts.Duration)
+		case op := <-bu.upserts:
+			bulk.Upsert(op.query, op.record)
+			ops++
+
+			if ops >= bu.opts.Count {
+				_, err := bulk.Run()
+				catcher.Add(err)
+				ops = 0
+			}
+		case f := <-bu.flusher:
+			select {
+			case last := <-bu.upserts:
+				bulk.Upsert(last.query, last.record)
+				ops++
+			default:
+			}
+
+			if ops > 0 {
+				_, err := bulk.Run()
+				catcher.Add(err)
+				ops = 0
+
+				f <- err
+				if !timer.Stop() {
+					<-timer.C
+				}
+				timer.Reset(bu.opts.Duration)
+			}
+
+			close(f)
+		case c := <-bu.closer:
+			close(bu.upserts)
+			for last := range bu.upserts {
+				bulk.Upsert(last.query, last.record)
+			}
+
+			if ops > 0 {
+				_, err := bulk.Run()
+				catcher.Add(err)
+			}
+
+			c <- catcher.Resolve()
+			close(c)
+			bu.cancel = nil
+			break bufferLoop
+		}
+	}
+
+	close(bu.err)
+}
+
+func (bu *anserBufUpsertImpl) Close() error {
+	if bu.cancel == nil {
+		return nil
+	}
+
+	res := make(chan error)
+	bu.closer <- res
+
+	bu.cancel()
+	bu.cancel = nil
+
+	return <-res
+}
+
+func (bu *anserBufUpsertImpl) Append(doc interface{}) error {
+	if doc == nil {
+		return errors.New("cannot insert a nil document")
+	}
+
+	switch d := doc.(type) {
+	case bson.RawD:
+		id, ok := getDocID(d)
+		if !ok {
+			return errors.New("could not find document ID")
+		}
+
+		bu.upserts <- upsertOp{
+			query:  Document{"_id": id},
+			record: d,
+		}
+	case map[string]interface{}:
+		id, ok := d["_id"]
+		if !ok {
+			return errors.New("could not find document ID")
+		}
+
+		bu.upserts <- upsertOp{
+			query:  Document{"_id": id},
+			record: d,
+		}
+	case Document:
+		id, ok := d["_id"]
+		if !ok {
+			return errors.New("could not find document ID")
+		}
+
+		bu.upserts <- upsertOp{
+			query:  Document{"_id": id},
+			record: d,
+		}
+	case bson.M:
+		id, ok := d["_id"]
+		if !ok {
+			return errors.New("could not find document ID")
+		}
+
+		bu.upserts <- upsertOp{
+			query:  Document{"_id": id},
+			record: d,
+		}
+	case map[string]string:
+		id, ok := d["_id"]
+		if !ok {
+			return errors.New("could not find document ID")
+		}
+
+		bu.upserts <- upsertOp{
+			query:  Document{"_id": id},
+			record: d,
+		}
+	default:
+		return errors.Errorf("document %T is not of a supported type", d)
+	}
+
+	return nil
+}
+
+func getDocID(rawDoc bson.RawD) (interface{}, bool) {
+	for _, raw := range rawDoc {
+		if raw.Name == "_id" {
+			return raw.Value, true
+		}
+	}
+	return nil, false
+
+}
+
+func (bu *anserBufUpsertImpl) Flush() error {
+	res := make(chan error)
+	bu.flusher <- res
+	return <-res
+}
---
+++ db/buffered_upsert_test.go
@@ -0,0 +1,247 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/mongodb/grip"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/suite"
+	mgo "gopkg.in/mgo.v2"
+)
+
+type BufferedUpsertSuite struct {
+	dbname  string
+	session *mgo.Session
+	uuid    uuid.UUID
+	db      Database
+	bi      *anserBufUpsertImpl
+	suite.Suite
+}
+
+func TestBufferedUpsertSuite(t *testing.T) {
+	suite.Run(t, new(BufferedUpsertSuite))
+}
+
+func (s *BufferedUpsertSuite) SetupSuite() {
+	var err error
+	s.uuid, err = uuid.NewV4()
+	s.Require().NoError(err)
+	s.dbname = fmt.Sprintf("anser_%s", s.uuid)
+	s.session, err = mgo.Dial("mongodb://localhost:27017")
+	s.Require().NoError(err)
+	s.db = WrapSession(s.session).DB(s.dbname)
+}
+
+func (s *BufferedUpsertSuite) SetupTest() {
+	s.bi = &anserBufUpsertImpl{
+		upserts: make(chan upsertOp, 1),
+		err:     make(chan error),
+		flusher: make(chan chan error),
+		closer:  make(chan chan error),
+		db:      WrapSession(s.session).DB(s.dbname),
+	}
+}
+
+func (s *BufferedUpsertSuite) takedown(collection string) {
+	s.NoError(s.session.DB(s.dbname).C(collection).DropCollection())
+}
+
+func (s *BufferedUpsertSuite) kickstart(ctx context.Context, collection string) {
+	ctx, s.bi.cancel = context.WithCancel(ctx)
+	s.bi.opts.Collection = collection
+	s.bi.opts.DB = s.dbname
+
+	if s.bi.opts.Count == 0 {
+		s.bi.opts.Count = 10
+	}
+
+	if s.bi.opts.Duration == 0 {
+		s.bi.opts.Duration = 100 * time.Millisecond
+	}
+
+	go s.bi.start(ctx)
+}
+
+func (s *BufferedUpsertSuite) TearDownSuite() {
+	s.Require().NoError(s.session.DB(s.dbname).DropDatabase())
+}
+
+func (s *BufferedUpsertSuite) TestAppendErrorsForNilDocuments() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	coll := s.uuid.String()
+	s.kickstart(ctx, coll)
+	defer s.takedown(coll)
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			s.NoError(s.bi.Append(Document{"_id": i + 1, "a": i}))
+			continue
+		}
+		s.Error(s.bi.Append(nil))
+	}
+	s.NoError(s.bi.Flush())
+
+	num, err := s.db.C(coll).Count()
+	s.NoError(err)
+	s.Equal(50, num)
+}
+
+func (s *BufferedUpsertSuite) TestBasicUpsertsBufferDoesNotFlushOnCancel() {
+	ctx, cancel := context.WithCancel(context.Background())
+	coll := s.uuid.String()
+	s.kickstart(ctx, coll)
+	defer s.takedown(coll)
+
+	for i := 0; i < 1000; i++ {
+		s.NoError(s.bi.Append(Document{"_id": i + 1, "a": i}))
+	}
+	cancel()
+
+	num, err := s.db.C(coll).Count()
+	s.NoError(err)
+	s.NotEqual(1000, num)
+}
+
+func (s *BufferedUpsertSuite) TestBufferFlushes() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	coll := s.uuid.String()
+	s.kickstart(ctx, coll)
+	defer s.takedown(coll)
+
+	for i := 0; i < 100; i++ {
+		s.bi.Append(Document{"_id": i + 1, "a": i})
+	}
+	s.NoError(s.bi.Flush())
+
+	num, err := s.db.C(coll).Count()
+	s.NoError(err)
+	s.Equal(100, num)
+}
+
+func (s *BufferedUpsertSuite) TestCloserFlushes() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	coll := s.uuid.String()
+	s.bi.opts.Count = 100000
+	s.bi.opts.Duration = time.Hour
+	s.kickstart(ctx, coll)
+	defer s.takedown(coll)
+
+	jobSize := 1000
+	for i := 0; i < jobSize; i++ {
+		s.bi.Append(Document{"_id": i + 1, "a": i})
+	}
+	s.NoError(s.bi.Close())
+
+	num, err := s.db.C(coll).Count()
+	s.NoError(err)
+	s.Equal(jobSize, num)
+}
+
+func (s *BufferedUpsertSuite) TestShouldNoopUsusally() {
+	for i := 0; i < 100; i++ {
+		s.NoError(s.bi.Close())
+	}
+
+	// now we try with a configured sender
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	coll := s.uuid.String()
+	s.kickstart(ctx, coll)
+	// no teardown because nothing happens
+	// defer s.takedown(coll)
+	for i := 0; i < 100; i++ {
+		s.NoError(s.bi.Close())
+	}
+	num, err := s.db.C(coll).Count()
+	s.NoError(err)
+	s.Equal(0, num)
+}
+
+func (s *BufferedUpsertSuite) closeWithPendingDocuments() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	coll := s.uuid.String()
+	s.bi.opts.Count = 100000
+	s.bi.opts.Duration = time.Hour
+	s.kickstart(ctx, coll)
+	defer s.takedown(coll)
+
+	catcher := grip.NewBasicCatcher()
+	jobSize := 10
+	for i := 0; i < jobSize; i++ {
+		s.bi.Append(Document{"_id": 1 + i, "a": i})
+	}
+
+	s.NoError(s.bi.Close())
+	s.NoError(catcher.Resolve())
+
+	num, err := s.db.C(coll).Count()
+	s.NoError(err)
+	s.Equal(jobSize, num)
+}
+
+func (s *BufferedUpsertSuite) flushWithPendingDocuments() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	coll := s.uuid.String()
+	s.bi.opts.Count = 100000
+	s.bi.opts.Duration = time.Hour
+	s.kickstart(ctx, coll)
+	defer s.takedown(coll)
+
+	catcher := grip.NewBasicCatcher()
+	jobSize := 10
+	for i := 0; i < jobSize; i++ {
+		s.bi.Append(Document{"_id": 1 + i, "a": i})
+	}
+
+	for i := 0; i < jobSize; i++ {
+		s.NoError(s.bi.Flush())
+	}
+
+	s.NoError(catcher.Resolve())
+
+	num, err := s.db.C(coll).Count()
+	s.NoError(err)
+	s.Equal(jobSize, num)
+}
+
+func (s *BufferedUpsertSuite) TestCloseWithPending00() { s.closeWithPendingDocuments() }
+func (s *BufferedUpsertSuite) TestCloseWithPending01() { s.closeWithPendingDocuments() }
+func (s *BufferedUpsertSuite) TestCloseWithPending02() { s.closeWithPendingDocuments() }
+func (s *BufferedUpsertSuite) TestCloseWithPending03() { s.closeWithPendingDocuments() }
+func (s *BufferedUpsertSuite) TestCloseWithPending04() { s.closeWithPendingDocuments() }
+
+func (s *BufferedUpsertSuite) TestFlushWithPending00() { s.flushWithPendingDocuments() }
+func (s *BufferedUpsertSuite) TestFlushWithPending01() { s.flushWithPendingDocuments() }
+func (s *BufferedUpsertSuite) TestFlushWithPending02() { s.flushWithPendingDocuments() }
+func (s *BufferedUpsertSuite) TestFlushWithPending03() { s.flushWithPendingDocuments() }
+func (s *BufferedUpsertSuite) TestFlushWithPending04() { s.flushWithPendingDocuments() }
+
+func (s *BufferedUpsertSuite) TestFlushBeforeTimerExpires() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	coll := s.uuid.String()
+	s.bi.opts.Count = 100000
+	s.bi.opts.Duration = time.Nanosecond
+	s.kickstart(ctx, coll)
+	defer s.takedown(coll)
+
+	jobSize := 100
+	for i := 0; i < jobSize; i++ {
+		s.bi.Append(Document{"_id": 2 + i, "a": i})
+	}
+
+	s.NoError(s.bi.Flush())
+
+	num, err := s.db.C(coll).Count()
+	s.NoError(err)
+	s.Equal(jobSize, num)
+}
