c0ade3adf3c3229a4fe89540ad97ad685f75c7ca
Sam Kleinman <sam@10gen.com>
2019-09-03 20:35:39 -0400

    MAKE-877: test new driver migration code (#25)

--- mock/client.go
+++ mock/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/mongodb/anser/client"
+	"github.com/mongodb/ftdc/bsonx"
 )
 
 type Client struct {
@@ -47,7 +48,7 @@ func (d *Database) Collection(name string) client.Collection {
 		return coll
 	}
 
-	d.Collections[name] = &Collection{CollName: name}
+	d.Collections[name] = &Collection{CollName: name, SingleResult: NewSingleResult()}
 	return d.Collections[name]
 }
 func (d *Database) RunCommand(ctx context.Context, cmd interface{}) client.SingleResult { return nil }
@@ -56,7 +57,11 @@ func (d *Database) RunCommandCursor(ctx context.Context, cmd interface{}) (clien
 }
 
 type Collection struct {
-	CollName string
+	CollName         string
+	UpdateResult     client.UpdateResult
+	SingleResult     *SingleResult
+	InsertManyResult client.InsertManyResult
+	InsertOneResult  client.InsertOneResult
 }
 
 func (c *Collection) Name() string { return c.CollName }
@@ -67,21 +72,62 @@ func (c *Collection) Find(ctx context.Context, query interface{}) (client.Cursor
 	return nil, nil
 }
 func (c *Collection) FindOne(ctx context.Context, query interface{}) client.SingleResult {
-	return nil
+	return c.SingleResult
 }
 func (c *Collection) InsertOne(ctx context.Context, doc interface{}) (*client.InsertOneResult, error) {
-	return nil, nil
+	return &c.InsertOneResult, nil
 }
 func (c *Collection) InsertMany(ctx context.Context, docs []interface{}) (*client.InsertManyResult, error) {
-	return nil, nil
+	return &c.InsertManyResult, nil
 }
 
 func (c *Collection) ReplaceOne(ctx context.Context, query, update interface{}) (*client.UpdateResult, error) {
-	return nil, nil
+	return &c.UpdateResult, nil
 }
 func (c *Collection) UpdateOne(ctx context.Context, query, update interface{}) (*client.UpdateResult, error) {
-	return nil, nil
+	return &c.UpdateResult, nil
 }
 func (c *Collection) UpdateMany(ctx context.Context, query, update interface{}) (*client.UpdateResult, error) {
-	return nil, nil
+	return &c.UpdateResult, nil
 }
+
+type Cursor struct {
+	CurrentValue   []byte
+	AllError       error
+	CloseError     error
+	DecodeError    error
+	ErrError       error
+	CursorID       int64
+	NextCallsCount int
+	MaxNextCalls   int
+}
+
+func (c *Cursor) Current() []byte                               { return c.CurrentValue }
+func (c *Cursor) All(ctx context.Context, in interface{}) error { return c.AllError }
+func (c *Cursor) Close(ctx context.Context) error               { return c.CloseError }
+func (c *Cursor) Decode(in interface{}) error                   { return c.DecodeError }
+func (c *Cursor) Err() error                                    { return c.ErrError }
+func (c *Cursor) ID() int64                                     { return c.CursorID }
+func (c *Cursor) Next(ctx context.Context) bool {
+	c.NextCallsCount++
+
+	return c.MaxNextCalls > c.NextCallsCount
+}
+
+type SingleResult struct {
+	DecodeError      error
+	DecodeBytesError error
+	DecodeBytesValue []byte
+	ErrorValue       error
+}
+
+func NewSingleResult() *SingleResult {
+	doc := bsonx.NewDocument()
+	val, _ := doc.MarshalBSON()
+
+	return &SingleResult{DecodeBytesValue: val}
+}
+
+func (sr *SingleResult) Decode(in interface{}) error  { return sr.DecodeError }
+func (sr *SingleResult) DecodeBytes() ([]byte, error) { return sr.DecodeBytesValue, sr.DecodeBytesError }
+func (sr *SingleResult) Err() error                   { return sr.ErrorValue }
