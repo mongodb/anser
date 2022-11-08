ae9b6949fd79d6501068e760684269d654f27db8
John Daniels <john.daniels@mongodb.com>
2022-11-08 11:37:49 -0500

    EVG-15746: Don't use ftdc anymore.
    We weren't using the ftdc monitor, and it was having issues with
    data races, so we go ahead and remove the code here.

--- apm/wrappers.go
+++ apm/wrappers.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"time"
 
-	"github.com/mongodb/ftdc"
 	"github.com/mongodb/grip"
 	"github.com/mongodb/grip/recovery"
 )
@@ -36,38 +35,6 @@ func (m *loggingMonitor) flusher(ctx context.Context) {
 			return
 		case <-ticker.C:
 			grip.Info(m.Monitor.Rotate().Message())
-		}
-	}
-}
-
-type ftdcCollector struct {
-	collector ftdc.Collector
-	interval  time.Duration
-	Monitor
-}
-
-// NewFTDCMonitor produces a monitor implementation that automatically
-// flushes the event data to an FTDC data collector.
-func NewFTDCMonitor(ctx context.Context, dur time.Duration, collector ftdc.Collector, m Monitor) Monitor {
-	impl := &ftdcCollector{
-		interval:  dur,
-		collector: collector,
-		Monitor:   m,
-	}
-	go impl.flusher(ctx)
-	return impl
-}
-
-func (m *ftdcCollector) flusher(ctx context.Context) {
-	defer recovery.LogStackTraceAndContinue("timeseries driver apm collector")
-	ticker := time.NewTicker(m.interval)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-ticker.C:
-			grip.Warning(m.collector.Add(m.Monitor.Rotate().Document()))
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/evergreen-ci/birch v0.0.0-20220401151432-c792c3d8e0eb
 	github.com/evergreen-ci/tarjan v0.0.0-20170824211642-fcd3f3321826
 	github.com/mongodb/amboy v0.0.0-20220408143015-94858bb64f00
-	github.com/mongodb/ftdc v0.0.0-20220401165013-13e4af55e809
 	github.com/mongodb/grip v0.0.0-20220401165023-6a1d9bb90c21
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.8.1
